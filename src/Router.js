58082cae7ed57d1fdff73cd596e8811bff3122a5
Emin Saygı <emin_0001@hotmail.com>
2022-03-05 22:25:09 +0300

    March 5, 2022 10:25 PM

--- src/Router.js
+++ src/Router.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import Jobs from './Pages/Jobs';
-import Detail from './Pages/Details';
 
-const Stack = createNativeStackNavigator();
-const Drawer = createDrawerNavigator();
+import JobsStack from './Navigations/JobsStack';
 
 const Router = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen
-          name="JobsDrawerPage"
-          component={Jobs}
-          options={{title: 'Jobs'}}></Drawer.Screen>
-        <Drawer.Screen
-          name="DetailsDrawerPage"
-          component={Detail}
-          options={{title: 'Details'}}></Drawer.Screen>
-      </Drawer.Navigator>
+      <JobsStack></JobsStack>
     </NavigationContainer>
   );
 };
