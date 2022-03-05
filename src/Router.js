eccce1cbf627a3bf1863d1b071c991d8ad126d4c
Emin Saygı <emin_0001@hotmail.com>
2022-03-05 20:25:00 +0300

    March 5, 2022 8:25 PM

--- src/Router.js
+++ src/Router.js
@@ -3,19 +3,10 @@ import {SafeAreaView, Text, StyleSheet} from 'react-native';
 
 const Router = () => {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView >
       <Text>Ana Sayfa</Text>
     </SafeAreaView>
   );
 };
 
 export default Router;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
