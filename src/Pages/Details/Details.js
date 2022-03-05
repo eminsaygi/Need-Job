df6d364f51656d692c474afeebd3b02842017a31
Emin Saygı <emin_0001@hotmail.com>
2022-03-05 22:40:13 +0300

    March 5, 2022 10:40 PM

--- src/Pages/Details/Details.js
+++ src/Pages/Details/Details.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Button} from 'react-native';
 
-const Details = () => {
+const Details = ({navigation}) => {
   return (
-    <View>
-      <Text>Details</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('FavoritesPage')}
+        title="Favori Sayfasını Aç"
+      />
     </View>
   );
 };
