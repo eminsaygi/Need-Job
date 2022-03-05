8cf9446aa4e2109a85647e547a776077ffb36028
Emin Saygı <emin_0001@hotmail.com>
2022-03-05 20:29:41 +0300

    March 5, 2022 8:29 PM

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {SafeAreaView, Text, StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Jobs from './Pages/Jobs';
+const Stack = createNativeStackNavigator();
 
 const Router = () => {
   return (
-    <SafeAreaView >
-      <Text>Ana Sayfa</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Jobs" component={Jobs} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
