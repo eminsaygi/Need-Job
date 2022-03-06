4c8fa0e4d9b8e6c7806c5f7e9cac71f47f1bca72
Emin Saygı <emin_0001@hotmail.com>
2022-03-06 09:51:41 +0300

    March 6, 2022 9:51 AM

---
+++ src/Components/JobCard/JobCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {useEffect, useState} from 'react';
+import {View, Text, FlatList, TouchableOpacity} from 'react-native';
+
+const JobCard = () => {
+  return (
+    <View>
+      <Text>JobCard</Text>
+    </View>
+  );
+};
+export default JobCard;
