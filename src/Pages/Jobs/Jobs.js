e8908534c19be30ee771b3bc927262fe1d996662
Emin Saygı <emin_0001@hotmail.com>
2022-03-06 09:54:29 +0300

    March 6, 2022 9:54 AM

--- src/Pages/Jobs/Jobs.js
+++ src/Pages/Jobs/Jobs.js
@@ -1,15 +1,18 @@
-import React,{useEffect,useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, SafeAreaView, FlatList} from 'react-native';
 import Config from '.react-native-config';
 
-import Error from '../../Assets/Animations/error.json'
-import JobCard from '../../Components/JobCard'
-import PageButton from '../../Components/Buttons'
-import useFetch from '../../Hooks/useFetch'
+import Error from '../../Assets/Animations/error.json';
+import JobCard from '../../Components/JobCard';
+import PageButton from '../../Components/Buttons';
+import useFetch from '../../Hooks/useFetch';
 
-import Styles from './Jobs.style'
-import Loading from '../../Components/Loading'
+import Styles from './Jobs.style';
+import Loading from '../../Components/Loading';
 const Jobs = ({navigation}) => {
+  const [page, setPage] = useState(1);
+  const {data, error, loading} = useFetch(`${Config.API_URL}?page=${page}`);
+
   return (
     <SafeAreaView>
       <FlatList
