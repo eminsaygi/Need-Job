8b8db8f3e6b97bc1f15498fe7ae2bcc49b642b31
Emin Saygı <emin_0001@hotmail.com>
2022-03-06 13:00:18 +0300

    March 6, 2022 1:00 PM

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {Provider} from 'react-redux';
 
 import JobsStack from './Navigations/JobsStack';
+import Stores from './Context/Store';
 
 const Router = () => {
   return (
-    <NavigationContainer>
-      <JobsStack></JobsStack>
-    </NavigationContainer>
+    <Provider store={Stores}>
+      <NavigationContainer>
+        <JobsStack></JobsStack>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
