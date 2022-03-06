8567f1e5f61045f09658fa11274b1df7f5d17476
Emin Saygı <emin_0001@hotmail.com>
2022-03-06 13:11:23 +0300

    March 6, 2022 1:11 PM

--- src/Pages/Details/Details.js
+++ src/Pages/Details/Details.js
@@ -1,14 +1,94 @@
 import React from 'react';
-import {View, Button} from 'react-native';
+import {
+  ScrollView,
+  Dimensions,
+  SafeAreaView,
+  Text,
+  View,
+  Linking,
+} from 'react-native';
+import Config from 'react-native-config';
+import RenderHTML from 'react-native-render-html';
+import {useDispatch} from 'react-redux';
 
-const Details = ({navigation}) => {
+import {addFavorite} from '../../Context/favoritesSlice';
+import Error from '../../Components/Error';
+import Loading from '../../Components/Loading';
+import PageButton from '../../Components/Buttons';
+import useFetch from '../../Hooks';
+
+import Styles from './Details.style';
+
+const Header = ({job}) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('FavoritesPage')}
-        title="Favori Sayfasını Aç"
-      />
+    <View style={Styles.headerContainer}>
+      <Text style={Styles.title}>{job.name}</Text>
+      <Text style={Styles.locations}>
+        <TextColor text="Location :" />{' '}
+        {job.locations && job.locations.length > 0
+          ? job.locations[0].name
+          : 'No information'}
+      </Text>
+      <Text style={Styles.levels}>
+        <TextColor text="Job Level :" />{' '}
+        {job.levels && job.levels.length > 0
+          ? job.levels[0].name
+          : 'No information'}
+      </Text>
+      <Text style={Styles.header}>Job Detail</Text>
     </View>
+  );
+};
+
+const Buttons = ({onPress, onSubmit}) => {
+  return (
+    <View style={Styles.buttons}>
+      <PageButton onPress={onSubmit} icon="login" text="Submit" />
+      <PageButton onPress={onPress} icon="heart" text="Favorite Job" />
+    </View>
+  );
+};
+
+const TextColor = ({text}) => {
+  return <Text style={Styles.text}>{text}</Text>;
+};
+
+const Details = ({route}) => {
+  const {id} = route.params;
+  const {data, error, loading} = useFetch(`${Config.JOBS_API_URL}/${id}`);
+  const {width} = Dimensions.get('window');
+  const dispatch = useDispatch();
+
+  if (error) {
+    return <Error />;
+  }
+  if (loading) {
+    return <Loading />;
+  }
+
+  const source = {
+    html: data.contents,
+  };
+
+  const addFavoriteList = job => {
+    dispatch(addFavorite(job));
+  };
+
+  const handleSubmit = item => {
+    Linking.openURL(item.refs.landing_page);
+  };
+
+  return (
+    <SafeAreaView style={Styles.container}>
+      <Header job={data} />
+      <ScrollView style={Styles.html} showsVerticalScrollIndicator={false}>
+        <RenderHTML source={source} contentWidth={width} />
+      </ScrollView>
+      <Buttons
+        onSubmit={() => handleSubmit(data)}
+        onPress={() => addFavoriteList(data)}
+      />
+    </SafeAreaView>
   );
 };
 
