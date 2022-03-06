8c706f31f9f0bcdacde2dbe208f08a3f7ea4a0de
Emin Saygı <emin_0001@hotmail.com>
2022-03-06 13:42:56 +0300

    March 6, 2022 1:42 PM

--- src/Context/Store.js
+++ src/Context/Store.js
@@ -1,8 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit';
-import favoritesReducer from './favoritesSlice';
+//import favoritesReducer from './favoritesSlice';
 
 export default configureStore({
   reducer: {
-    favorites: favoritesReducer,
+    // favorites: favoritesReducer,
   },
 });
