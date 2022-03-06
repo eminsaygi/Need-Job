ffe476aae6a7fd90b7d777d7014266dae758a085
Emin Saygı <emin_0001@hotmail.com>
2022-03-06 13:03:38 +0300

    March 6, 2022 1:03 PM

---
+++ src/Pages/Jobs/Jobs.style.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f2f2f2',
+  },
+  footerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+});
