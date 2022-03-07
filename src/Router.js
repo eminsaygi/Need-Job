add45910981057479dc146a13e728012fa617fd4
Emin Saygı <emin_0001@hotmail.com>
2022-03-07 18:13:26 +0300

    March 7, 2022 6:13 PM

--- src/Router.js
+++ src/Router.js
@@ -5,15 +5,17 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import JobsStack from './Navigations/JobsStack';
 import Stores from './Context/store';
-import Login from './Pages/Login'
+import Login from './Pages/Login';
 
 const Stack = createNativeStackNavigator();
-
 
 const Router = () => {
   return (
     <Provider store={Stores}>
       <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="LoginPage" component={Login}></Stack.Screen>
+        </Stack.Navigator>
         <JobsStack></JobsStack>
       </NavigationContainer>
     </Provider>
