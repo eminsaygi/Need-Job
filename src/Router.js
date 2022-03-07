b20dcd50745c8bad6c0867170fb1c083f3f4715f
Emin Saygı <emin_0001@hotmail.com>
2022-03-07 18:12:19 +0300

    March 7, 2022 6:12 PM

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import JobsStack from './Navigations/JobsStack';
 import Stores from './Context/store';
+import Login from './Pages/Login'
+
+const Stack = createNativeStackNavigator();
+
 
 const Router = () => {
   return (
