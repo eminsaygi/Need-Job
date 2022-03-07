2ac3b97808e7dff27ce9e5c5ded3218aca0f8d20
Emin Saygı <emin_0001@hotmail.com>
2022-03-07 20:54:21 +0300

    March 7, 2022 8:54 PM

---
+++ src/Components/Buttons/LoginButton/LoginButton.style.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#2286c3',
+    borderRadius: 5,
+    borderColor: '#e6e6e6',
+    padding: 10,
+    margin: 10,
+  },
+  title: {fontWeight: 'bold', fontSize: 16, color: 'white'},
+});
