d6e07eee09cf9b6486c4865849d8802304b38e50
Emin Saygı <emin_0001@hotmail.com>
2022-03-07 18:04:50 +0300

    March 7, 2022 6:04 PM

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,3 +2,21 @@ import React from 'react';
 import {SafeAreaView, Image, View, Alert} from 'react-native';
 import {Formik} from 'formik';
 import {useDispatch} from 'react-redux';
+
+import Styles from './Login.style';
+
+import Config from 'react-native-config';
+
+import usePost from '../../Hooks/usePost'
+
+const Login = ({navigation}) => {
+    const {data,loading,error,post} = usePost();
+
+    function handleLogin(){
+        post(Config.API_AUTH_URL+ '/login', values)
+    }
+
+    if (error) {
+        Alert.alert('Error', error.message);
+    }
+}
