4d9626b9534d40c928f052a34f14523fc83c117f
Emin Saygı <emin_0001@hotmail.com>
2022-03-07 21:08:38 +0300

    March 7, 2022 9:08 PM

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -19,7 +19,7 @@ const Login = ({navigation}) => {
   if (error) {
     Alert.alert('Error', 'Bir Hata oldu');
   }
-  if (data.status === 'Error') {
+  if (data.status === null) {
     Alert.alert('Error', 'Kullanıcı Bulunamadı');
   } else {
     navigation.navigate('JobsPage');
