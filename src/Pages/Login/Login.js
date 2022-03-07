b5bea5de61ab6363c2661c5f0154c6c60239f6d0
Emin Saygı <emin_0001@hotmail.com>
2022-03-07 18:09:16 +0300

    March 7, 2022 6:09 PM

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -7,16 +7,65 @@ import Styles from './Login.style';
 
 import Config from 'react-native-config';
 
-import usePost from '../../Hooks/usePost'
+import usePost from '../../Hooks/usePost';
 
 const Login = ({navigation}) => {
-    const {data,loading,error,post} = usePost();
+  const {data, loading, error, post} = usePost();
 
-    function handleLogin(){
-        post(Config.API_AUTH_URL+ '/login', values)
-    }
+  function handleLogin() {
+    post(Config.API_AUTH_URL + '/login', values);
+  }
 
-    if (error) {
-        Alert.alert('Error', error.message);
-    }
-}
+  if (error) {
+    Alert.alert('Error', 'Bir Hata oldu');
+  }
+  if (data.status === 'Error') {
+    Alert.alert('Error', 'Kullanıcı Bulunamadı');
+  } else {
+    navigation.navigate('JobsPage');
+  }
+
+  return (
+    <SafeAreaView style={Styles.container}>
+      <View style={Styles.logoContainer}>
+        <Image
+          style={Styles.logo}
+          source={require('../../assets/images/logo.png')}
+        />
+      </View>
+      <Formik
+        initialValues={{email: '', password: ''}}
+        onSubmit={values => {
+          handleLogin();
+        }}>
+        {({handleChange, handleBlur, handleSubmit, values}) => (
+          <View style={Styles.formContainer}>
+            <View style={Styles.inputContainer}>
+              <Text style={Styles.label}>Email</Text>
+              <TextInput
+                style={Styles.input}
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+              />
+            </View>
+            <View style={Styles.inputContainer}>
+              <Text style={Styles.label}>Password</Text>
+              <TextInput
+                style={Styles.input}
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+              />
+            </View>
+            <View style={Styles.buttonContainer}>
+              <Button title="Login" onPress={handleSubmit} />
+            </View>
+          </View>
+        )}
+      </Formik>
+    </SafeAreaView>
+  );
+};
+
+export default Login;
